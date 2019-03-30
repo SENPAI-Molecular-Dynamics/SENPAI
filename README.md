50fad8cd53d553061dd3c6f9e4ef087f11677841
Garuda1 <garuda1@protonmail.com>
2019-03-30 18:49:16 +0100

    update

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # Ammonium
 
-Ammonium is a terminal-based molecular simulation software for large-scale workloads.
+Ammonium is a terminal-based molecular simulation software for large-scale workloads. Reading an initial state from a file, Ammonium outputs a `.csv` log for every simulated particle, allowing the user to analyse the behaviour of a system in extreme details.
 
-![pic1](https://i.imgur.com/92crMr8.png)
+#Compilation
 
-Reading an initial state from any file, Ammonium outputs a `.csv` log for every simulated particle, allowing the user to analyse the behaviour of a system in extreme details.
+Ammonium requires the C standard library. No additional software is required. Compilation is achieved using the provided makefile.
 
-![pic1](https://i.imgur.com/FHaSzOg.png)
+#Usage
+
+`ammonium -i input_file -o output_file [options]`
+
+Options:
+
+`-i | --in` Required. Specifies the path to the input file.
+`-o | --out` Required. Specifies the name prefix for the output file. using `-o out` with two particles would tell Ammonium to create `out0.csv` and `out1.csv`.
+`--grav nb` Specifies the value for the gravitational coupling constant. (default: SI value)
+`--elec nb` Specifies the value for the electromagnetic coupling constant. (default: SI value)
+`--time t` Specifies the time at which the simulation stops. (default: 1 nanosecond)
+`--dt t` Specifies the numerical integration constant (default: 1 picosecond)
--- sources/vec3d.c
+++ sources/vec3d.c
@@ -15,7 +15,7 @@
 t_vec3d *vec3d_add(t_vec3d *dest, const t_vec3d *v1, const t_vec3d *v2)
 {
   if (dest == NULL || v1 == NULL || v2 == NULL)
-    return (retstr(NULL, TEXT_VEC3D_ADD_FAILURE, __FILE__, __LINE__));
+    return (retstr(NULL, TEXT_CANTMATH, __FILE__, __LINE__));
 
   dest->x = (v1->x + v2->x);
   dest->y = (v1->y + v2->y);
@@ -26,7 +26,7 @@ t_vec3d *vec3d_add(t_vec3d *dest, const t_vec3d *v1, const t_vec3d *v2)
 t_vec3d *vec3d_sub(t_vec3d *dest, const t_vec3d *v1, const t_vec3d *v2)
 {
   if (dest == NULL || v1 == NULL || v2 == NULL)
-    return (retstr(NULL, TEXT_VEC3D_SUB_FAILURE, __FILE__, __LINE__));
+    return (retstr(NULL, TEXT_CANTMATH, __FILE__, __LINE__));
 
   dest->x = (v1->x - v2->x);
   dest->y = (v1->y - v2->y);
@@ -37,7 +37,7 @@ t_vec3d *vec3d_sub(t_vec3d *dest, const t_vec3d *v1, const t_vec3d *v2)
 t_vec3d *vec3d_mul(t_vec3d *dest, const t_vec3d *v, const double lambda)
 {
   if (dest == NULL || v == NULL)
-    return (retstr(NULL, TEXT_VEC3D_MUL_FAILURE, __FILE__, __LINE__));
+    return (retstr(NULL, TEXT_CANTMATH, __FILE__, __LINE__));
 
   dest->x = (v->x * lambda);
   dest->y = (v->y * lambda);
@@ -48,9 +48,9 @@ t_vec3d *vec3d_mul(t_vec3d *dest, const t_vec3d *v, const double lambda)
 t_vec3d *vec3d_div(t_vec3d *dest, const t_vec3d *v, const double lambda)
 {
   if (dest == NULL || v == NULL)
-    return (retstr(NULL, TEXT_VEC3D_DIV_FAILURE, __FILE__, __LINE__));
+    return (retstr(NULL, TEXT_CANTMATH, __FILE__, __LINE__));
   else if (lambda < 10E-50)
-    return (retstr(NULL, TEXT_VEC3D_DIV_FAILURE, __FILE__, __LINE__));
+    return (retstr(NULL, TEXT_CANTMATH, __FILE__, __LINE__));
 
   dest->x = (v->x / lambda);
   dest->y = (v->y / lambda);
@@ -61,7 +61,7 @@ t_vec3d *vec3d_div(t_vec3d *dest, const t_vec3d *v, const double lambda)
 double vec3d_dot(const t_vec3d *v1, const t_vec3d *v2)
 {
   if (v1 == NULL || v2 == NULL)
-    return (retstri(-1.0, TEXT_VEC3D_DOT_FAILURE, __FILE__, __LINE__));
+    return (retstri(-1.0, TEXT_CANTMATH, __FILE__, __LINE__));
 
   return ((v1->x * v2->x) + (v1->y * v2->y) + (v1->z * v2->z));
 }
@@ -69,7 +69,7 @@ double vec3d_dot(const t_vec3d *v1, const t_vec3d *v2)
 double vec3d_mag(const t_vec3d *v)
 {
   if (v == NULL)
-    return (retstri(-1.0, TEXT_VEC3D_MAG_FAILURE, __FILE__, __LINE__));
+    return (retstri(-1.0, TEXT_CANTMATH, __FILE__, __LINE__));
 
   return (sqrt((v->x * v->x)+(v->y * v->y)+(v->z * v->z)));
 }
@@ -77,7 +77,7 @@ double vec3d_mag(const t_vec3d *v)
 t_vec3d *vec3d_cross(t_vec3d *dest, const t_vec3d *v1, const t_vec3d *v2)
 {
   if (dest == NULL || v1 == NULL || v2 == NULL)
-    return (retstr(NULL, TEXT_VEC3D_CROSS_FAILURE, __FILE__, __LINE__));
+    return (retstr(NULL, TEXT_CANTMATH, __FILE__, __LINE__));
 
   dest->x = (v1->y * v2->z) - (v1->z * v2->y);
   dest->y = (v1->z * v2->x) - (v1->x * v2->z);
@@ -91,15 +91,15 @@ t_vec3d *vec3d_unit(t_vec3d *dest, const t_vec3d *v)
   t_vec3d *res;
 
   if (dest == NULL || v == NULL)
-    return (retstr(NULL, TEXT_VEC3D_UNIT_FAILURE, __FILE__, __LINE__));
+    return (retstr(NULL, TEXT_CANTMATH, __FILE__, __LINE__));
 
   if ((mag = vec3d_mag(v)) < 0.0)
-    return (retstr(NULL, TEXT_VEC3D_UNIT_FAILURE, __FILE__, __LINE__));
+    return (retstr(NULL, TEXT_CANTMATH, __FILE__, __LINE__));
   if ((res = vec3d_div(dest, v, mag)) == NULL)
-    return (retstr(NULL, TEXT_VEC3D_UNIT_FAILURE, __FILE__, __LINE__));
+    return (retstr(NULL, TEXT_CANTMATH, __FILE__, __LINE__));
 
   if (res == NULL)
-    return (retstr(NULL, TEXT_VEC3D_UNIT_FAILURE, __FILE__, __LINE__));
+    return (retstr(NULL, TEXT_CANTMATH, __FILE__, __LINE__));
 
   return (res);
 }
